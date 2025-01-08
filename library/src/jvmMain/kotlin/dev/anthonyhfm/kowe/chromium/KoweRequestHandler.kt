6efa250f5e75bbdd58cd3d972efa130afedb4ef1
Anthony Hofmeister <anthonyhofmeister@gmx.de>
2025-01-08 19:14:57 +0100

    RequestHandler for custom policies

---
+++ library/src/jvmMain/kotlin/dev/anthonyhfm/kowe/chromium/KoweRequestHandler.kt
@@ -0,0 +1,27 @@
+package dev.anthonyhfm.kowe.chromium
+
+import dev.anthonyhfm.kowe.data.WebPolicy
+import org.cef.browser.CefBrowser
+import org.cef.browser.CefFrame
+import org.cef.handler.CefRequestHandlerAdapter
+import org.cef.network.CefRequest
+
+class KoweRequestHandler : CefRequestHandlerAdapter() {
+    var policy: WebPolicy? = null
+
+    override fun onBeforeBrowse(
+        browser: CefBrowser?,
+        frame: CefFrame?,
+        request: CefRequest?,
+        user_gesture: Boolean,
+        is_redirect: Boolean
+    ): Boolean {
+        var cancel = false
+
+        if (policy != null && request?.url != null) {
+            cancel = policy!!.decidePolicyFor(request.url)
+        }
+
+        return cancel
+    }
+}
